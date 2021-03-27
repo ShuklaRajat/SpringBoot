67f64e9d1843ee51b94b3d3399b22aacb3f232eb
Rajat Shukla <rajats.think@gmail.com>
2021-03-27 13:30:19 +0530

    When tried to access localhost:8080 able to get 'Hello World'

--- demo_1/src/main/java/com/example/demo/DemoApplication.java
+++ demo_1/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+   // We have to get something from our server, so use @GetMapping annotation. @RestController allows this class to be
+   // exposed as a REST endpoint. And currently we have only one REST endpoint which is hello() which  returns
+   // "Hello World".
+   @GetMapping
+	public String hello() {
+		return "Hello World";
+	}
 }
