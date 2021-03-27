576049c46e647feaff7a32b634b8f2fa2ba03a2b
Rajat Shukla <rajats.think@gmail.com>
2021-03-27 14:03:06 +0530

    Created a basic Student REST API.

--- demo_1/src/main/java/com/example/demo/DemoApplication.java
+++ demo_1/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -17,7 +23,13 @@ public class DemoApplication {
    // exposed as a REST endpoint. And currently we have only one REST endpoint which is hello() which  returns
    // "Hello World".
    @GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return Arrays.asList(new Student(
+				1L,
+				"Rajat",
+				"rajat@springboot.com",
+				LocalDate.of(1998, Month.JUNE, 16),
+				23
+		));
 	}
 }
